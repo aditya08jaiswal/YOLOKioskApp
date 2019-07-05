cc29e6304103bdb2e733d87293fe86a9ac3e2b8c
iam844 <jaiswaladitya844@gmail.com>
2019-07-05 14:34:53 +0530

    Basic health checkup UI

---
+++ app/src/main/java/com/yolohealth/yolokioskapp/BasicHealthCheckupActivity.java
@@ -0,0 +1,17 @@
+package com.yolohealth.yolokioskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicHealthCheckupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_health_checkup);
+
+
+
+    }
+}
