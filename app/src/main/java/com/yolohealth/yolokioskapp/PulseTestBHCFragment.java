6034a5d5bd813cc35388117839d57a69fe8d2c20
iam844 <jaiswaladitya844@gmail.com>
2019-07-06 18:00:34 +0530

    Add manual entry to Pulse test

--- app/src/main/java/com/yolohealth/yolokioskapp/PulseTestBHCFragment.java
+++ app/src/main/java/com/yolohealth/yolokioskapp/PulseTestBHCFragment.java
@@ -8,7 +8,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import pl.droidsonroids.gif.GifImageView;
 
@@ -18,6 +20,8 @@ public class PulseTestBHCFragment extends Fragment {
     GifImageView instructionAudioGif;
     MediaPlayer mediaPlayer;
 //    ImageView playAudioImage;
+    Button pulseTestManualEntryBtn;
+    LinearLayout pulseTestMainFragmentLayout, pulseTestManualEntryLayout;
 
     public PulseTestBHCFragment() {
         // Required empty public constructor
@@ -39,6 +43,9 @@ public class PulseTestBHCFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_pulse_test_bhc, container, false);
         instructionAudioGif = view.findViewById(R.id.instruction_playing_image_gif);
 //        playAudioImage = view.findViewById(R.id.pulse_test_play_iv);
+        pulseTestManualEntryBtn = view.findViewById(R.id.pulse_test_manual_entry_button);
+        pulseTestMainFragmentLayout = view.findViewById(R.id.pulse_test_main_fragment_layout);
+        pulseTestManualEntryLayout = view.findViewById(R.id.pulse_test_manual_entry_layout);
 
         try{
             mediaPlayer.start();
@@ -49,6 +56,16 @@ public class PulseTestBHCFragment extends Fragment {
             public void onClick(View v) {
                 mediaPlayer.start();
 //                playAudioImage.setVisibility(View.GONE);
+            }
+        });
+
+        pulseTestManualEntryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pulseTestMainFragmentLayout.setVisibility(View.GONE);
+                pulseTestManualEntryLayout.setVisibility(View.VISIBLE);
+
             }
         });
 
